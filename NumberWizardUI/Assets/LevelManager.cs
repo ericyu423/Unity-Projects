d7a522978c209d1d399cd610f18790ca90706ff9
ericyu423 <ericyu423@hotmail.com>
2017-10-05 15:43:03 -0700

    Add files via upload

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log("love load requested for: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitGame ()
+	{
+
+		//Application.Quit; 
+	
+	}
+
+
+
+
+
+
+}
---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAllowed = 10;
+	// Use this for initialization
+
+	public Text text; //type Text name text  in swift public var text: Text!
+	void Start () {
+		
+		StartGame ();
+
+	}
+
+	void StartGame ()
+	{
+		max = 1000;
+		min = 1;
+		nextGuess();
+
+
+
+
+
+	}
+
+
+	public void GuessHigher ()
+	{
+		min = guess;
+		nextGuess();
+
+	}
+
+	public void GuessLower ()
+	{
+		max = guess;
+		nextGuess();
+	}
+
+
+
+	public void nextGuess ()
+	{
+		guess = Random.Range(min,max+1);
+		//guess = (min + max) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+
+			SceneManager.LoadScene("Win");
+		}
+
+	}
+
+
+
+
+}
